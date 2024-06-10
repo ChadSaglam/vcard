4dca3ebd1a23b49ecaa73a18b8e5f1f916d0cea2
Chad Saglam <saglam.chad@chadev.ch>
2024-06-10 15:05:25 +0200

    vcard basic project with qr code

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,280 @@
-import Image from "next/image";
+"use client"
 
+import QRCode from 'qrcode.react';
+import { useState } from "react";
+
+interface VCardFormValues {
+  firstName: string;
+  lastName: string;
+  orgTitle: string;
+  orgName: string;
+  orgStreet: string;
+  orgCity: string;
+  orgRegion: string;
+  orgPost: string;
+  orgCountry: string;
+  orgTel: string;
+  orgEmail: string;
+  orgUrl: string;
+  profilePic: string,
+}
+
+const initialFormValues: VCardFormValues = {
+  firstName: '',
+  lastName: '',
+  orgTitle: '',
+  orgName: '',
+  orgStreet: '',
+  orgCity: '',
+  orgRegion: '',
+  orgPost: '',
+  orgCountry: '',
+  orgTel: '',
+  orgEmail: '',
+  orgUrl: '',
+  profilePic: '',
+};
 export default function Home() {
+  const [formValues, setFormValues] = useState<VCardFormValues>(initialFormValues);
+  const [vcardString, setVcardString] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const generateVcard = () => {
+    let vcard = 'BEGIN:VCARD\nVERSION:3.0\n';
+
+    vcard += `N:${formValues.lastName};${formValues.firstName}\n`;
+    vcard += `FN:${formValues.firstName} ${formValues.lastName}\n`;
+
+    // Work information
+    if (formValues.orgName) {
+      vcard += `ORG:${formValues.orgName}\n`;
+    }
+    if (formValues.orgTitle) {
+      vcard += `TITLE:${formValues.orgTitle}\n`;
+    }
+    if (formValues.orgStreet || formValues.orgCity || formValues.orgRegion || formValues.orgPost || formValues.orgCountry) {
+      vcard += `ADR;TYPE=work:;;${formValues.orgStreet};${formValues.orgCity};${formValues.orgRegion};${formValues.orgPost};${formValues.orgCountry}\n`;
+    }
+    if (formValues.orgTel) {
+      vcard += `TEL;TYPE=work:${formValues.orgTel}\n`;
+    }
+    if (formValues.orgEmail) {
+      vcard += `EMAIL;TYPE=internet,work:${formValues.orgEmail}\n`;
+    }
+    if (formValues.orgUrl) {
+      vcard += `URL;TYPE=work:${formValues.orgUrl}\n`;
+    }
+
+    // Add profile picture
+    // if (formValues.profilePic) {
+    //   vcard += `PHOTO;TYPE=JPEG:${'https://avatars.githubusercontent.com/u/142899363?v=4.jpg'}\n`; 
+    // }
+
+    // Add profile picture
+    // if (formValues.profilePic) {
+    //   vcard += `LOGO;TYPE=PNG:${formValues.profilePic}\n`; 
+    // }
+
+    console.log(vcard)
+
+    vcard += 'END:VCARD';
+    setVcardString(vcard);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <div className="flex flex-row w-full">
+        <div className="flex flex-col w-3/4">
+          <form className="flex flex-col gap-4" onSubmit={(e) => {
+            e.preventDefault();
+            generateVcard();
+          }}>
+            {/* <label htmlFor="profilePic" className="text-gray-700 font-medium">Profile Picture:</label>
+            <div className="flex flex-col ">
+              <input
+                type="url"
+                id="profilePic"
+                name="profilePic"
+                value={formValues.profilePic}
+                placeholder='URL'
+                onChange={handleChange}
+                className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+              />
+            </div> */}
+          
+            <label htmlFor="firstName" className="text-gray-700 font-medium">Full Name</label>
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={formValues.firstName}
+                  placeholder='First Name'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={formValues.lastName}
+                  placeholder='Last Name'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <label htmlFor="firstName" className="text-gray-700 font-medium">Work Details</label>
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  id="orgTitle"
+                  name="orgTitle"
+                  value={formValues.orgTitle}
+                  placeholder='Job Title'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="text"
+                  id="orgName"
+                  name="orgName"
+                  value={formValues.orgName}
+                  placeholder='Organization Name'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  id="orgStreet"
+                  name="orgStreet"
+                  value={formValues.orgStreet}
+                  placeholder='Street Address'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="text"
+                  id="orgCity"
+                  name="orgCity"
+                  value={formValues.orgCity}
+                  placeholder='City'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  id="orgRegion"
+                  name="orgRegion"
+                  value={formValues.orgRegion}
+                  placeholder='Region'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="text"
+                  id="orgPost"
+                  name="orgPost"
+                  value={formValues.orgPost}
+                  placeholder='Postal Code'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  id="orgCountry"
+                  name="orgCountry"
+                  value={formValues.orgCountry}
+                  placeholder='Country'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="tel"
+                  id="orgTel"
+                  name="orgTel"
+                  value={formValues.orgTel}
+                  placeholder='Phone Number'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row w-full">
+              <div className="flex flex-col w-full">
+                <input
+                  type="email"
+                  id="orgEmail"
+                  name="orgEmail"
+                  value={formValues.orgEmail}
+                  placeholder='Email Address'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex flex-col pl-3 w-full">
+                <input
+                  type="url"
+                  id="orgUrl"
+                  name="orgUrl"
+                  value={formValues.orgUrl}
+                  placeholder='Website'
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Generate vCard
+            </button>
+          </form>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col w-1/4 pl-9">
+          <h3 className="flex flex-col pb-12">QR code</h3>
+          <div className="flex justify-center py-32">
+            {vcardString && 
+              <QRCode value={vcardString} size={256} />
+            }
+          </div>
+        </div>
       </div>
     </main>
   );
